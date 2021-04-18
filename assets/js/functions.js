0189c59a169703f491a6427d86256059e668a75e
LCDS97 <lucas.cds1997@gmail.com>
2021-04-18 12:25:11 -0300

    Refazendo projeto
    Refazendo projeto, vou utilizar somente Node.JS e HTML/CSS/JS
    Retirei o pug

--- assets/js/functions.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-    // $("#loading").hide()
-    // $("#wrapper").hide()
-
-    $("#loading").fadeIn("slow").delay(1500).fadeOut("slow")
-    $("#wrapper").delay(2500).fadeIn("slow")
-});
