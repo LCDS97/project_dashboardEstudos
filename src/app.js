0702fb1182c36fd6653672a85a4f1b9e955954f5
Lucas Conceição dos Santos <lucas.cds1997@gmail.com>
2021-05-29 01:12:41 -0300

    Init of back-end structure

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'))
+
+const homeRoute = require('./routes/home-route')
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}))
+
+app.use('/', homeRoute);
+
+module.exports = app;
